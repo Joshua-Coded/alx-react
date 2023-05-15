bae1debe1eb3e0babab01b6afb239055cd2961e3
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-05-15 20:46:33 +0200

    task 1

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,18 @@
+const $ = require('jquery');
+const _ = require('lodash');
+
+let count = 0;
+
+$('<p>Holberton Dashboard</p>').appendTo('body');
+$('<p>Dashboard data for the students</p>').appendTo('body');
+$('<button>Click here to get started</p>').appendTo('body');
+$('<p id="count"></p>').appendTo('body');
+$('<p>Copyright - Holberton School</p>').appendTo('body');
+
+
+function updateCounter() {
+    counter++;
+    $('#count').html(`${counter} clicks on the button`);
+}
+
+$("button").on("click", _.debounce(updateCounter, 500));
