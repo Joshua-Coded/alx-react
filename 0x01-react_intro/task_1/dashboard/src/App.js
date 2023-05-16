5d18c548297ceb027c96aa40bf72528a97398085
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-05-16 20:17:10 +0200

    task_1

--- 0x01-react_intro/task_1/dashboard/src/App.js
+++ 0x01-react_intro/task_1/dashboard/src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.jpg';
 import { getFullYear, getFooterCopy } from './utils';
 import './App.css';
-
+import Notifications from './Notifications';
 function App() {
   return (
     <div className="App">
+
+      
+      
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>School dashboard</h1>
       </header>
+      <Notifications />
       <div className="App-body">
         <p>Login to access the full dashboard</p>
       </div>
--- 0x01-react_intro/task_1/dashboard/src/index.js
+++ 0x01-react_intro/task_1/dashboard/src/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+// import Notifications from './Notifications';
 import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App />   
+    
   </React.StrictMode>
 );
+
+ReactDOM.render(
+	<React.StrictMode>
+		
+	</React.StrictMode>,
+	document.getElementById('root-notifications')
+);
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
