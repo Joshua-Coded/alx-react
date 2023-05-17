7574a38f2d3307f9fcd4a6767e49d4b29c92bd27
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-05-17 22:18:47 +0200

    dashboard

--- 0x01-react_intro/task_5/dashboard/utils/utils.js
+++
@@ -1,9 +0,0 @@
-export const getFullYear = () => {
-    return new Date().getFullYear();
-}
-
-export const getFooterCopy = (isIndex) => {
-    return isIndex ? 'Holberton School' : 'Holberton School main dashboard';
-}
-
-export const getLatestNotification = () => "<strong> Urgent requirements</strong> - complete by EOD";
