f8e13d120ee189f21d9230abd06f7eedd8144456
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-05-25 09:35:12 +0200

    webpack

---
+++ 0x02-react_props/task_0/dashboard/config/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    entry: './src/index.js',
+    output: {
+	filename: 'bundle.js',
+	path: path.resolve('./dist'),
+    },
+    devServer: {
+	hot: true,
+	contentBase: path.resolve('./dist'),
+	compress: true,
+	port: 8564,
+    },
+    performance: {
+	maxAssetSize: 1000000,
+	maxEntrypointSize: 1000000,
+    },
+    module: {
+	rules: {
+	    test: /\.(js|jsx)$/,
+	    exclude: /node_modules/,
+	    loader: 'babel-loader',
+	},
+	{
+	    test: /\.(gif|png|jpe?g|svg)$/i,
+	    use: [
+		'file-loader',
+		{
+		    loader: 'image-webpack-loader',
+		    options: {
+			bypassOnDebug: true,
+			disable: true,
+		    },
+		},
+	    ],
+	},
+	
+    },
+}
