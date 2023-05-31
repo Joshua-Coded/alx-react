5946ba8b62a07cd378b043e6eaffc59d403db20e
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-05-31 10:50:46 +0200

    tasks

--- 0x04-React_inline_styling/task_4/dashboard/src/CourseList/CourseShape.js
+++
@@ -1,8 +0,0 @@
-import PropTypes from 'prop-types';
-
-export default PropTypes.shape({
-  id: PropTypes.number.isRequired,
-  name: PropTypes.string.isRequired,
-  credit: PropTypes.number.isRequired
-}
-);
--- 0x04-React_inline_styling/task_4/dashboard/src/Notifications/NotificationItemShape.js
+++
@@ -1,8 +0,0 @@
-import PropType from 'prop-types';
-
-export default PropType.shape({
-  id: PropType.number.isRequired,
-  html: PropType.shape({__html: PropType.string}),
-  type: PropType.string.isRequired,
-  value: PropType.string
-});
